d79f7835de755f3c9508c8e4d92baa7b4572c5fe
Mehdy-H-Asaad <mehdyasaad.sy.2003@gmail.com>
2025-07-06 14:40:37 +0400

    refactor(client/properties): Modified suggested properties  enabled and loading skeleton

--- client/src/features/properties/components/suggested-properties/suggested-properties.tsx
+++ client/src/features/properties/components/suggested-properties/suggested-properties.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import {
 	SuggestedPropertiesCard,
 	useGetSuggestedProperties,
@@ -11,11 +12,19 @@ export const SuggestedProperties = ({ propertyId }: { propertyId: string }) => {
 		<div className="mt-10">
 			<h1 className="text-3xl font-bold mb-8">Related Properties</h1>
 
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-				{suggestedProperties?.map(property => (
-					<SuggestedPropertiesCard key={property._id} {...property} />
-				))}
-			</div>
+			{isLoadingSuggestedProperties ? (
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4	">
+					{Array.from({ length: 4 }).map((_, index) => (
+						<Skeleton key={index} className="w-full h-[200px] rounded-lg" />
+					))}
+				</div>
+			) : (
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+					{suggestedProperties?.map(property => (
+						<SuggestedPropertiesCard key={property._id} {...property} />
+					))}
+				</div>
+			)}
 		</div>
 	);
 };
